65269550cc5bb8be321bbcd9aa383c6fa03ab1e6
Nejc Zupan <nejc.zupan@gmail.com>
2011-06-13 00:19:20 +0200

    more unit-tests for raptus.article.images

--- raptus/article/images/tests/test_images.py
+++ raptus/article/images/tests/test_images.py
@@ -76,6 +76,168 @@ class TestGetImagesIntegration(RAImagesIntegrationTestCase):
         self.assertEquals('image1 image2'.split(), [i.id for i in images])
 
 
+class TestGetImageURL(unittest.TestCase):
+    """Unit tests for logic of all edge cases in
+    raptus.article.images.images.Image.getImageURL().
+    """
+
+    def test_not_an_image(self):
+        """Test return value when there is no image."""
+
+        schema = {'image': mock.Mock(spec='get'.split())}
+        schema['image'].get.return_value = None
+
+        image = mock.Mock(spec='Schema'.split())
+        image.Schema.return_value = schema
+
+        from raptus.article.images.images import Image
+        self.assertEquals(None, Image(image).getImageURL())
+
+    @mock.patch('raptus.article.images.images.Image.getSize')
+    def test_w_and_h_not_set(self, getSize):
+        """Test return value when w and h are not set."""
+        getSize.return_value = 0, 0
+
+        schema = {'image': mock.Mock(spec='get'.split())}
+
+        image = mock.Mock(spec='Schema absolute_url'.split())
+        image.Schema.return_value = schema
+        image.absolute_url.return_value = 'http://plone/image'
+
+        from raptus.article.images.images import Image
+        self.assertEquals('http://plone/image/image', Image(image).getImageURL())
+
+    @mock.patch('raptus.article.images.images.Image.getSize')
+    @mock.patch('raptus.article.images.images.component')
+    def test_w_or_h_set(self, zope_component, getSize):
+        """Test return value when w or h are set."""
+
+        scale_url = 'http://plone/image/@@images/b16...fa2.jpeg'
+        zope_component.getMultiAdapter.return_value.scale.return_value.url = scale_url
+        getSize.return_value = 100, 100
+
+        schema = {'image': mock.Mock(spec='get'.split())}
+        image = mock.Mock(spec='Schema absolute_url REQUEST'.split())
+        image.Schema.return_value = schema
+        image.absolute_url.return_value = 'http://plone/image'
+
+        from raptus.article.images.images import Image
+        self.assertEquals(scale_url, Image(image).getImageURL())
+
+    @mock.patch('raptus.article.images.images.Image.getSize')
+    def test_size_is_used(self, getSize):
+        """Test that size parameter is used."""
+
+        getSize.return_value = 0, 0
+
+        schema = {'image': mock.Mock(spec='get'.split())}
+        image = mock.Mock(spec='Schema absolute_url'.split())
+        image.Schema.return_value = schema
+
+        from raptus.article.images.images import Image
+        Image(image).getImageURL(size='foo')
+        getSize.assert_called_one_with(size='foo')
+
+
+class TestGetImage(unittest.TestCase):
+    """Unit tests for logic of all edge cases in
+    raptus.article.images.images.Image.getImage().
+    """
+
+    @mock.patch('raptus.article.images.images.Image.getImageURL')
+    def test_no_url(self, getImageURL):
+        """Test return value when url is None."""
+
+        getImageURL.return_value = None
+
+        from raptus.article.images.images import Image
+        image = mock.Mock(spec=''.split())
+        self.assertEquals(None, Image(image).getImage())
+
+    @mock.patch('raptus.article.images.images.Image.getImageURL')
+    def test_size_is_used(self, getImageURL):
+        """Test that size parameter is used."""
+
+        getImageURL.return_value = None
+
+        from raptus.article.images.images import Image
+        image = mock.Mock(spec=''.split())
+        Image(image).getImage(size='foo')
+
+        getImageURL.assert_called_one_with(size='foo')
+
+    @mock.patch('raptus.article.images.images.Image.getImageURL')
+    @mock.patch('raptus.article.images.images.Image.getCaption')
+    def test_get_image_tag(self, getCaption, getImageURL):
+        """Test HTML image tag return value of getImage()."""
+
+        getImageURL.return_value = "http://plone/image"
+        getCaption.return_value = 'foo'
+
+        from raptus.article.images.images import Image
+        image = mock.Mock(spec=''.split())
+        self.assertEquals('<img src="http://plone/image" alt="foo" />', Image(image).getImage())
+
+
+class TestGetSize(unittest.TestCase):
+    """Unit tests for logic of all edge cases in
+    raptus.article.images.images.Image.getSize().
+    """
+
+    @mock.patch('raptus.article.images.images.getToolByName')
+    def test_get_size(self, getToolByName):
+        """Test return value of getSize()."""
+        from raptus.article.images.images import Image
+        getToolByName.return_value.raptus_article.getProperty.return_value = 9
+
+        # test result
+        image = mock.Mock(spec=''.split())
+        w, h = Image(image).getSize('foo')
+        self.assertEquals((9, 9), (w, h))
+
+        # test call parameters
+        call_args_list = getToolByName.return_value.raptus_article.getProperty.call_args_list
+        self.assertEquals(call_args_list[0], (('images_foo_width', 0), {}))
+        self.assertEquals(call_args_list[1], (('images_foo_height', 0), {}))
+
+
+class TestGetCaption(unittest.TestCase):
+    """Unit tests for logic of all edge cases in
+    raptus.article.images.images.Image.getCaption().
+    """
+
+    def test_title(self):
+        """Test return value when only title is set."""
+
+        image = mock.Mock(spec='Title Description'.split())
+        image.Title.return_value = 'foo'
+        image.Description.return_value = None
+
+        from raptus.article.images.images import Image
+        self.assertEquals('foo', Image(image).getCaption())
+
+    def test_description(self):
+        """Test return value when only description is set."""
+
+        image = mock.Mock(spec='Title Description'.split())
+        image.Title.return_value = None
+        image.Description.return_value = 'bar'
+
+        from raptus.article.images.images import Image
+        self.assertEquals('bar', Image(image).getCaption())
+
+    def test_title_and_description(self):
+        """Test that only description is returned when both title and
+        description are set."""
+
+        image = mock.Mock(spec='Title Description'.split())
+        image.Title.return_value = 'foo'
+        image.Description.return_value = 'bar'
+
+        from raptus.article.images.images import Image
+        self.assertEquals('bar', Image(image).getCaption())
+
+
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
     above.
