eb00f917dbc98980591b3d368494d04b72b5d688
Nejc Zupan <nejc.zupan@gmail.com>
2011-06-22 23:27:40 +0200

    polished interfaces.py: pep8 fixes, added shebang and module-level docstring

--- raptus/article/images/interfaces.py
+++ raptus/article/images/interfaces.py
@@ -1,43 +1,39 @@
+# -*- coding: utf-8 -*-
+"""Marker and definition interfaces."""
+
 from zope import interface
 
+
 class IImages(interface.Interface):
-    """ Provider for images contained in an article
-    """
-    
+    """Provider for images contained in an article."""
+
     def getImages(**kwargs):
-        """ Returns a list of images based on the criteria passed as kwargs (catalog brains)
-        """
+        """Returns a list of images based on the criteria passed as kwargs (catalog brains)."""
+
 
 class IImage(interface.Interface):
-    """ Handler for image thumbing and captioning
-    """
-        
+    """Provider for image thumbing and captioning."""
+
     def getImageURL(size="original"):
-        """
-        Returns the url to the image in the specific size
-        
+        """Returns the url to the image in the specific size.
+
         The sizes are taken from the raptus_article properties sheet
         and are formed by the following name schema:
-        
+
             images_<size>_(height|width)
         """
-    
+
     def getImage(size="orginal"):
-        """ 
-        Returns the html tag of the image in the specific size
-        
+        """Returns the html tag of the image in the specific size.
+
         The sizes are taken from the raptus_article properties sheet
         and are formed by the following name schema:
-        
+
             images_<size>_(height|width)
         """
-        
+
     def getSize(size):
-        """
-        Returns the width and height registered for the specific size
-        """
-    
+        """Returns the width and height registered for the specific size."""
+
     def getCaption():
-        """
-        Returns the caption for the image
-        """
+        """Returns the caption for the image."""
